ac87ba0be0003ed73e48c7ecd04b86aa507d4724
danielandrewr <70853459+danielandrewr@users.noreply.github.com>
2020-12-21 15:29:20 +0700

    Main file changed
    Banyak yang berubah

--- brokoli_proyek_akhir.c
+++ brokoli_proyek_akhir.c
@@ -2,68 +2,101 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-
-#define MAX_USER_INPUT 5
-
-void error_arguments();
+#include <windows.h>
+#include "brokoli.h"
 
 typedef struct {
 	char name[100];
 	int age;
-	char domisili[20];
-	bool isTestRequired;
+	char domisili[30];
+	int test_status;
+	int common_symptoms;
+	int uncommon_symptoms;
+	int serious_symptoms;
 } User;
 
-int main(int argc, char *argv[]) {
-	// File Handler
-	FILE *pFile;
-	pFile = fopen("user_database.txt", "w");
+int main(void) {
+	int MAX_USER_INPUT = 5; // default
+	int user_number;
+	char answer_buffer[1];
 	
-	// Untuk menghitung banyak data yang sudah tersimpan
-	int user_count = 0;
+	int input;
 	
-	if (pFile != NULL) {
-		if (argc == 3) {
-			int user_number = argc - 1;
-			if (strcmp(argv[3], "input") == 0) {
-				if (user_number < MAX_USER_INPUT) {
-					int i, j;
-					User user[user_number];
-					// Loop for inputting value to struct
-					for (i = 0; i < user_number; i++) {
-						int count_left = user_number - i;
-						printf("Ada %i data tersisa yang perlu diisi.\n", count_left);
-						
-						printf("Masukkan nama user\t: "); scanf("%[^/n]", &user[i].name);
-						printf("Masukkan umur user\t: "); scanf("%i", &user[i].age);
-						printf("Masukkan domisili user\t: "); scanf("%[^/n]", &user[i].domisili);
-						user[i].isTestRequired = false;
-					}
-					
-					// Loop for storing struct value to file
-					//for (i = 0; i < user_number; i++) {
-						
-					//}
+	menu:
+		main_menu();
+	
+	printf("Masukkan input action: ");
+	scanf("%i", &input);
+	
+	if (input == 1) {
+		goto input_data;
+	} else if (input == 2) {
+		goto lihat_data;
+	} else if (input == 0) {
+		return 0;
+	} else {
+		return 0;
+	}
+		
+	input_data:
+		system("cls");
+		printf("\nMasukkan banyak user yang ingin diinput (max: 5 user): "); scanf("%i", &user_number);
+		
+		if (user_number <= MAX_USER_INPUT) {	
+			User user[user_number];
+			int i, j;
+			while (i < user_number) {
+				int count_left = user_number - i;
+				printf("\nData Input User #%i\n", i+1);
+				printf("\n");
+				printf("Ada %i data tersisa yang perlu diisi.\n", count_left);
+				if (i >= 1) {
+					system("pause");
 				}
+				
+				getchar();
+				printf("Masukkan nama user\t: ");
+				gets(user[i].name);
+				
+				printf("Masukkan umur user\t: "); 
+				scanf("%i", &user[i].age);
+				
+				getchar();
+				printf("Masukkan domisili user\t: ");
+				gets(user[i].domisili);
+				fflush(stdin);
+				
+				user[i].test_status = 0;
+				user[i].common_symptoms = 0;
+				user[i].uncommon_symptoms = 0;
+				user[i].serious_symptoms = 0;
+				
+				i++;
 			}
 			
-		} else if (argc > 3) {
-			// Jika argument yang dimasukkan lebih dari dua
+			printf("\nOutput\n");
+			for (i = 0; i < user_number; i++) {
+				printf("\nData User #%i\n", i+1);
+				
+				printf("Nama\t\t: %s\n", user[i].name);
+				printf("Umur\t\t: %i\n", user[i].age);
+				printf("Domisili\t: %s\n", user[i].domisili);
+				
+			}
+		} else {
 			error_message();
-			printf("Error: Terlalu banyak argument yang dimasukkan!");
-			printf("Usage: ./<project_name> 'input|read|count' ");
+			printf("User tidak bisa melebihi batas yang ditentukan.");	
 		}
 		
-		//Close file syntax
-		fclose(pFile);
-	} else {
-		error_message();
-		printf("Could not open user_database.txt file!");
-	}
+		char answer;
+		make_question("\nAnda telah menyelesaikan input data, Apakah anda ingin melanjutkan ke main menu? (Y/N)\n", answer_buffer, sizeof(answer_buffer));
+		printf(">> %s", answer_buffer);
+		scanf("%s", &answer);
+		if (answer == 'Y') goto menu;
 	
+	
+	lihat_data:
+		return 1;
+			
 	return 0;
-}
-
-void error_message() {
-	printf("Terjadi kesalahan pada program:");
 }
