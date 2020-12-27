99dd289d298dc0ad6f4f44e7224c0e1b03067110
danielandrewr <70853459+danielandrewr@users.noreply.github.com>
2020-12-28 00:12:40 +0700

    added menu functionality

--- brokoli.h
+++ brokoli.h
@@ -2,13 +2,6 @@ void error_message() {
 	printf("\nTerjadi kesalahan pada program:\n");
 }
 
-int check_user_test_status(const char *answer, int common, int uncommon, int serious) {
-	int status = 0;
-	// write algorithms here
-	
-	return status;
-}
-
 bool check_input_validity(char input[2]) {
 	bool valid = true;
 	fgets(input, sizeof(input), stdin);
@@ -46,11 +39,34 @@ int make_question(const char *question, char *answer, const size_t answer_length
 
 void main_menu() {
 	system("cls");
-	printf("-=-=-=-=-=-=- <Program Name> -=-=-=-=-=-=-=\n");
-	printf("Tolong input nomor berikut ini untuk akses\n");
+	printf("-=-=-=-=-=-=- Proyek Akhir ProgDas -=-=-=-=-=-=-=\n");
+	printf("Harap input nomor berikut ini untuk akses\n");
 	printf("fitur-fitur di dalam program ini.\n");
 	printf("\n");
 	printf("1. Input Data\n");
 	printf("2. Lihat Data\n");
-	printf("-=-=-=-=-=-=- <Program Name> -=-=-=-=-=-=-=\n");
+	printf("3. Bantuan Penggunaan\n");
+	printf("4. Tentang Kelompok\n");
+	printf("0. Exit Program\n");
+	printf("-=-=-=-=-=-=- Proyek Akhir ProgDas -=-=-=-=-=-=-=\n");
+}
+
+void print_help() {
+	system("cls");
+	printf("Program ini ditujukan untuk menganalisis gejala user untuk\n");
+	printf("mendapatkan hasil apakah user memerlukan tes SWAB atau tidak.\n");
+	printf("Pembuatan program ini berdasarkan data yang didapatkan dari Kemenkes\n");
+	printf("dan WHO.\n");
+	printf("User dapat memasukkan data beberapa kasus sekaligus dan data akan\n");
+	printf("tersimpan di dalam database 'user.txt'.\n");
+	printf("Saat menjawab pertanyaan harap gunakan Y atau N (case sensitive).");
+}
+
+void print_credits() {
+	system("cls");
+	printf("Program ini dibuat oleh Kelompok Brokoli:\n");
+	printf("Aleandro Farrel Rivaldi / 2006577271\n");
+	printf("Josephus Daniel A. R / 2006577284\n");
+	printf("M. Darrel Tristan / 2006577302\n");
+	printf("Steven Yosua S. / ");
 }
